16aded192dc928274305361dc889cd60430cbb89
hyunjialee <148006117+hyunjialee@users.noreply.github.com>
2023-11-03 11:20:03 -0400

    Update ReadFour.java
    finished

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,5 +1,6 @@
 package Step4;
 
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
@@ -14,5 +15,28 @@ public class ReadFour
         {
             // Print out a running total of all the
             // values in the input file.
+            try {
+                //Create scanner and select input path
+                Scanner fileIn = new Scanner(new File("Step4/input.txt"));
+                //Create file to write into
+                PrintWriter fileOut = new PrintWriter(new File("Step4/output.txt"));
+
+                int sum = 0;
+                //Read and write the total
+                while (fileIn.hasNext()) {
+                    //Read next line
+                    String lineIn = fileIn.nextLine();
+
+                    //get new total
+                    sum += Integer.parseInt(lineIn);
+
+                    //Save to fileOut
+                    fileOut.println("Running total = " + sum);
+                }
+                fileOut.close();
+            }
+            catch (IOException e){
+                System.out.println("File not found, try again Corinne.");
         }
+    }
 }
